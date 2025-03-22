92bd0e00797244d9fef41c1dba5419cc3e83aae3
ubaydulloh07 <ubaydullohmirzaaxmedov@gmail.com>
2025-03-23 04:36:56 +0500

    add

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn.dummyjson.com"],
+    domains: ["i.dummyjson.com"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(ico|png|jpg|jpeg|gif|svg)$/,
+      type: "asset/resource",
+    });
+    return config;
   },
 };
 
---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '@/styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Online Do\'kon',
+  description: 'Online do\'kon mahsulotlari',
+  icons: {
+    icon: '/logo.png'
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="uz">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+} 
